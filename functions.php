eb539848291f7c91fd4d347bf940e39e6f1aa3d7
Jesper Orb <jesper.orb@zocom.se>
2018-04-26 10:19:33 +0200

    Init

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+//! Hook
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+function my_theme_enqueue_styles()
+{
+    $parent_style = 'parent-style';
+    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+
+add_action('wp_enqueue_scripts', 'add_custom_script');
+
+function add_custom_script()
+{
+    wp_enqueue_style('scripts', get_stylesheet_directory_uri() . '/js/scripts.js');
+}
